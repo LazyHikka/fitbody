f64cae3317054b54b1b3d4972be2c3596cc8a6c5
LazyHikka <yarka.trubka@gmail.com>
2024-06-27 00:44:42 +0300

    DB connected. Now working on auth

--- lib/fitbodyapp.dart
+++ lib/fitbodyapp.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:fitbody/router/router.dart';
 import 'package:fitbody/theme/theme.dart';
+import 'package:get_it/get_it.dart';
+import 'package:talker_flutter/talker_flutter.dart';
 
 class FitBody extends StatelessWidget {
   const FitBody({super.key});
@@ -11,6 +13,9 @@ class FitBody extends StatelessWidget {
     return MaterialApp(
       theme: darkTheme,
       routes: routes,
+      navigatorObservers: [
+        TalkerRouteObserver(GetIt.I<Talker>()),
+      ],
     );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,30 @@
+import 'dart:async';
+
 import 'package:firebase_core/firebase_core.dart';
+
 import 'package:fitbody/fitbodyapp.dart';
 import 'package:flutter/material.dart';
+import 'package:get_it/get_it.dart';
+import 'package:talker_flutter/talker_flutter.dart';
+
 import 'firebase_options.dart';
 
-Future<void> main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(const FitBody());
+
+  final talker = Talker();
+  GetIt.I.registerSingleton(talker);
+  GetIt.I<Talker>().debug('Talker started');
+  GetIt.I<Talker>().error('Talker started');
+  GetIt.I<Talker>().info('Talker started');
+
+  final app = await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  talker.info(app.options.projectId);
+
+  runZonedGuarded(
+    () => runApp(const FitBody()),
+    (e, st) => GetIt.I<Talker>().handle(e, st),
+  );
 }
